a75624c6a8a8d6944091230af0122768ca045e2b
tsu-nera <fox10225fox@gmail.com>
2022-02-23 08:03:20 +0900

    :sparkles:generate pom.xml using tools-pom

--- build.clj
+++ build.clj
@@ -1,7 +1,8 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [tools-pom.tasks :as pom]))
 
-(def lib 'tsu-nera/meingen-bot-firebase)
+(def lib 'tsu-nera/meingen-bot-gcloud-run)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
 
 (def target-dir "target")
@@ -54,3 +55,28 @@
            :uber-file uber-file
            :basis     basis
            }))
+
+(defn- set-opts [opts]
+  (assoc opts
+         :lib          lib
+         :version      version
+         :write-pom    true
+         ;; :validate-pom true
+                                        ; Note: this EDN can come from anywhere - you could externalise it into a separate edn file (e.g. pom.edn), synthesise it from information elsewhere in your project, or whatever other scheme you like
+         ;; :pom
+         ;; {:description      "Description of your project e.g. your project's GitHub \"short description\"."
+         ;;  :url              "https://github.com/yourusername/yourproject"
+         ;;  :licenses         [:license   {:name "Apache License 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}] ; Note first element is a tag
+         ;;  :developers       [:developer {:id "yourusername" :name "yourname" :email "youremail@emailservice.com"}]           ; And here
+         ;;  :scm              {:url                  "https://github.com/yourusername/yourproject"
+         ;;                     :connection           "scm:git:git://github.com/yourusername/yourproject.git"
+         ;;                     :developer-connection "scm:git:ssh://git@github.com/yourusername/yourproject.git"}
+         ;;  :issue-management {:system "github" :url "https://github.com/yourusername/yourproject/issues"}}
+         ))
+
+(defn pom
+  "Construct a comprehensive pom.xml file for this project"
+  [opts]
+  (-> opts
+      (set-opts)
+      (pom/pom)))
