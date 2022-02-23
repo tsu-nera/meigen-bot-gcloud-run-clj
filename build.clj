fc18c8b2a18542ad8f0e589b948e7c560f83ccad
tsu-nera <fox10225fox@gmail.com>
2022-02-23 16:54:20 +0900

    :construction:wip

--- build.clj
+++ build.clj
@@ -1,6 +1,5 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]
-            [tools-pom.tasks :as pom])
+  (:require [clojure.tools.build.api :as b])
   (:import
    [com.google.cloud.tools.jib.api Jib Containerizer RegistryImage TarImage])
   )
@@ -14,24 +13,15 @@
 (def jar-file (format "%s/%s-%s-application.jar" artifacts-dir (name lib) version))
 (def uber-file (format "%s/application.jar" artifacts-dir))
 
-(def src-clj ["src/main/clojure"])
-(def src-java ["src/main/java"])
-(def src [src-clj src-java])
+(def src ["src/main/clojure"])
 (def basis (b/create-basis {:project "deps.edn"}))
 
 ;; clj -T:build clean
 (defn clean [_]
-  "delete the build target directory"
+  ;; "delete the build target directory"
   (println (str "Cleaning... " target-dir))
   (b/delete {:path "target"}))
 
-(defn compile-java [_]
-  (println (str "compiling... " src-java))
-  (b/javac {:src-dirs   src-java
-            :class-dir  class-dir
-            :basis      basis
-            :javac-opts ["-source" "11" "-target" "11"]
-            }))
 
 ;; clj -T:build jar
 (defn jar [_]
@@ -39,8 +29,8 @@
                 :lib       lib
                 :version   version
                 :basis     basis
-                :src-dirs  src-clj})
-  (b/copy-dir {:src-dirs   src-clj
+                :src-dirs  src})
+  (b/copy-dir {:src-dirs   src
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
           :jar-file  jar-file}))
@@ -48,17 +38,30 @@
 ;; clj -T:build uber
 (defn uber [_]
   (clean nil)
-  (compile-java nil)
-  (b/copy-dir {:src-dirs   src-clj
+  (b/copy-dir {:src-dirs   src
                :target-dir class-dir})
   (b/compile-clj {:basis     basis
-                  :src-dirs  src-clj
+                  :src-dirs  src
                   :class-dir class-dir})
   (b/uber {:class-dir class-dir
            :uber-file uber-file
            :basis     basis
            }))
 
+;; 1. Compile the code and generate a pom.xml
+(defn prepare [_]
+  (b/write-pom {:class-dir class-dir
+                :lib       lib
+                :version   version
+                :basis     basis
+                :src-dirs  src})
+  (b/compile-clj {:basis     basis
+                  :src-dirs  src
+                  :class-dir class-dir}))
+
+;; 2. Containerize with Jib and upload the container to registry
+;; (defn jib [_]
+;;   nil)
 
 ;; (defn jib [_]
 ;;   (.containerize
@@ -74,40 +77,3 @@
 ;;           Containerizer.to(RegistryImage.named("gcr.io/my-project/hello-from-jib")
 ;;                                             .addCredential("myusername", "mypassword")));
 ;; )
-
-
-;; (defn pom [_]
-;;   (println "generating pom.xml...")
-;;   (b/write-pom {:class-dir class-dir
-;;                 :lib       lib
-;;                 :version   version
-;;                 :basis     basis
-;;                 :src-dirs  src-clj}))
-
-(defn- set-opts [opts]
-  (assoc opts
-         :lib          lib
-         :version      version
-         :write-pom    true
-         :validate-pom true
-
-         ;; Note: this EDN can come from anywhere - you could externalise it into a separate edn file (e.g. pom.edn), synthesise it from information elsewhere in your project, or whatever other scheme you like
-         ;; :pom
-         ;; {:description      "Description of your project e.g. your project's GitHub \"short description\"."
-         ;;  :url              "https://github.com/yourusername/yourproject"
-         ;;  :licenses         [:license   {:name "Apache License 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}] ; Note first element is a tag
-         ;;  :developers       [:developer {:id "yourusername" :name "yourname" :email "youremail@emailservice.com"}]           ; And here
-         ;;  :scm              {:url                  "https://github.com/yourusername/yourproject"
-         ;;                     :connection           "scm:git:git://github.com/yourusername/yourproject.git"
-         ;;                     :developer-connection "scm:git:ssh://git@github.com/yourusername/yourproject.git"}
-         ;;  :issue-management {:system "github" :url "https://github.com/yourusername/yourproject/issues"}}
-         ;;
-         )
-  )
-
-(defn pom
-  "Construct a comprehensive pom.xml file for this project"
-  [opts]
-  (-> opts
-      (set-opts)
-      (pom/pom)))
--- src/main/java/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-import nl.epij.gcf.RingHttpFunction;
-
-public class HelloWorld extends RingHttpFunction {
-  public String getHandler() { return "functions.helloworld/app"; }
-}
